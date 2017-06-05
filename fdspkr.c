8942b449856f904491e9bf14c30dca71f8f0ce02
Jan Havran <havran.jan@email.cz>
2017-06-05 17:27:58 +0200

    Register module as input sound device

--- fdspkr.c
+++ fdspkr.c
@@ -9,13 +9,55 @@
  */
 
 #include <linux/module.h>
+#include <linux/input.h>
 
 #define DRIVER_NAME	"fdspkr"
 #define DRIVER_VERSION	"0.1"
 #define DRIVER_LICENSE	"GPL v2"
 
+struct input_dev *fdspkr_input;
+
+static int fdspkr_open(struct input_dev *dev)
+{
+	printk("Open\n");
+
+	return 0;
+}
+
+static void fdspkr_close(struct input_dev *dev)
+{
+	printk("Close\n");
+}
+
+static int fdspkr_event(struct input_dev *dev, unsigned int type,
+unsigned int code, int value)
+{
+	printk("type %u, code %u, value %d\n", type, code, value);
+
+	return 0;
+}
+
 static int __init fdspkr_init(void)
 {
+	int ret;
+
+	fdspkr_input = input_allocate_device();
+	if (!fdspkr_input)
+		return -ENOMEM;
+
+	fdspkr_input->name = "Floppy Disk Speaker";
+	fdspkr_input->evbit[0] = BIT(EV_SND);
+	fdspkr_input->sndbit[0] = BIT(SND_BELL) | BIT(SND_TONE);
+	fdspkr_input->open = fdspkr_open;
+	fdspkr_input->close = fdspkr_close;
+	fdspkr_input->event = fdspkr_event;
+
+	ret = input_register_device(fdspkr_input);
+	if (ret) {
+		input_free_device(fdspkr_input);
+		return ret;
+	}
+
 	printk("FD Speaker init\n");
 
 	return 0;
@@ -23,6 +65,8 @@ static int __init fdspkr_init(void)
 
 static void __exit fdspkr_exit(void)
 {
+	input_unregister_device(fdspkr_input);
+
 	printk("FD Speaker exit\n");
 }
 
